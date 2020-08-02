01bc7027d178c270bcd4d53bc5f0e65e03a02759
cort zhang <learn_zhang@163.com>
2020-08-02 21:50:38 +0800

    Signed-off-by: cort zhang <learn_zhang@163.com>

--- Cort_TomcatProject/src/test/UserTest.java
+++ Cort_TomcatProject/src/test/UserTest.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 public class UserTest {
     @Test
     public void userTest(){
-//        User user = new User(1,"张三","123456","male");
+        User user = new User(2,"李四","123456","male");
         System.out.println(new User(1,"张三","123456","male"));
+		System.out.println(user);
     }
 }
